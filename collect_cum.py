5eb538f74db99949c141bb9ed20a7ab2e3eb22b2
Denis <117019627+DENIS76T5@users.noreply.github.com>
2023-06-09 10:18:40 -0700

    imba

---
+++ collect_cum.py
@@ -0,0 +1,14 @@
+class Start_masturmate:
+    "lets go"
+class Collect:
+    def user_cum():
+        "bring pear to the your python"
+class User:
+    def cum():
+        with open("cum.mp4", "r") as cum:
+            pass
+            
+def stop():
+    "pover of"
+def increase_speed():
+    "add lubricant"
---
+++ process.py
@@ -0,0 +1,11 @@
+from collect_cum import *
+Start_masturmate()
+
+if User.cum:
+    pear = Collect.user_cum()
+    stop()
+else:
+    increase_speed()
+    
+
+
